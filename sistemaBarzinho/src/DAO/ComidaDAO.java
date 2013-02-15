7e2ad7bf910836cb1a42c414b3690d0c74acaec2
julianaIora <julianaiora@hotmail.com>
2013-02-15 19:31:47 -0200

    #33 criação do DAO para comida

---
+++ sistemaBarzinho/src/DAO/ComidaDAO.java
@@ -0,0 +1,115 @@
+package DAO;
+
+import Modelo.Comida;
+import Utilitarios.Utilitarios;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ComidaDAO extends DBConnection {
+
+    private static Connection conn;
+    private static PreparedStatement ps;
+    private static String sqlInsert = "INSERT INTO Comidas(nome,quantidade,data_validade,`tipo`) VALUES(?,?,?,?)";
+    private static String sqlUpdate = "UPDATE Comidas SET nome = ?, quantidade = ?, data_validade = ?, `tipo` = ? WHERE id = ?";
+    private static String sqlDelete = "DELETE FROM Comidas WHERE id = ?";
+    private static String sqlFind = "SELECT id, nome,quantidade,data_validade,`tipo` FROM Comidas WHERE id = ?";
+    private static String sqlList = "SELECT id, nome,quantidade,data_validade,`tipo` FROM Comidas";
+    private static ResultSet rs;
+
+    public static void insert(Comida comida) throws SQLException {
+        try {
+            conn = connect();
+            ps = conn.prepareStatement(sqlInsert, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, comida.getNome());
+            ps.setInt(2, comida.getQuantidade());
+            ps.setString(3, Utilitarios.dateToString(comida.getDataValidade()));
+            ps.setString(4, comida.getTipo());
+            ps.execute();
+
+        } finally {
+            ps.close();
+            closeConnect(conn);
+        }
+    }
+
+    public static void update(Comida comida) throws SQLException {
+        try {
+            conn = connect();
+            ps = conn.prepareStatement(sqlUpdate, PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, comida.getNome());
+            ps.setInt(2, comida.getQuantidade());
+            ps.setString(3, Utilitarios.dateToString(comida.getDataValidade()));
+            ps.setString(4, comida.getTipo());
+            ps.setInt(5, comida.getId());
+            ps.execute();
+        } finally {
+            ps.close();
+            closeConnect(conn);
+        }
+    }
+
+    public static void delete(int idComida) throws SQLException {
+        try {
+            conn = connect();
+            ps = conn.prepareStatement(sqlDelete);
+            ps.setInt(1, idComida);
+            ps.execute();
+        } finally {
+            ps.close();
+            closeConnect(conn);
+        }
+    }
+
+    public static ArrayList<Comida> list() throws SQLException {
+        ArrayList<Comida> comidas = new ArrayList<Comida>();
+        try {
+            conn = connect();
+            ps = conn.prepareStatement(sqlList);
+            rs = ps.executeQuery();
+            Comida comida = null;
+
+            while (rs.next()) {
+                comida = new Comida();
+                comida.setId(rs.getInt("id"));
+                comida.setNome(rs.getString("nome"));
+                comida.setQuantidade(rs.getInt("quantidade"));
+                comida.setDataValidade(rs.getDate("data_validade"));
+                comidas.add(comida);
+            }
+
+            return comidas;
+
+        } finally {
+            ps.close();
+            rs.close();
+            closeConnect(conn);
+        }
+    }
+
+    public static Comida find(int idBebida) throws SQLException {
+        try {
+            conn = connect();
+            ps = conn.prepareStatement(sqlFind);
+            ps.setInt(1, idBebida);
+            rs = ps.executeQuery();
+            Comida comida = null;
+
+            if (rs.next()) {
+                comida = new Comida();
+                comida.setId(rs.getInt("id"));
+                comida.setNome(rs.getString("nome"));
+                comida.setQuantidade(rs.getInt("quantidade"));
+                comida.setDataValidade(rs.getDate("data_validade"));
+            }
+            return comida;
+
+        } finally {
+            ps.close();
+            rs.close();
+            closeConnect(conn);
+        }
+    }
+}
---
+++ sistemaBarzinho/src/Modelo/Comida.java
@@ -0,0 +1,52 @@
+package Modelo;
+
+import java.util.Date;
+
+public class Comida {
+
+    private int id;
+    private String nome;
+    private int quantidade;
+    private Date dataValidade;
+    private String tipo;
+
+    public Date getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(Date dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
