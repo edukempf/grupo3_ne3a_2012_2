20b5fb45f673c002eb98bae53379873db9b17474
julianaIora <julianaiora@hotmail.com>
2013-02-15 14:13:20 -0200

    #19 Criação da classe de conexão

---
+++ sistemaBarzinho/src/DAO/DBConnection.java
@@ -0,0 +1,38 @@
+package DAO;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DBConnection {
+
+    private static String host = "localhost";
+    private static String login = "root";
+    private static String password = "root";
+    private static String port = "3306";
+    private static String database = "aluno";
+
+    public static Connection getMyDBConnection() throws SQLException, ClassNotFoundException, IOException {
+        Class.forName("com.mysql.jdbc.Driver");
+        String x = "jdbc:mysql://" + host + ":" + port + "/" + database;
+        return DriverManager.getConnection(x, login, password);
+    }
+
+    public static Connection connect() {
+        try {
+            return getMyDBConnection();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void closeConnect(Connection con) {
+        try {
+            con.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
