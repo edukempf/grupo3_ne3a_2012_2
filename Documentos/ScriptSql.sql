05507a22c222fc56d0fce3b1f89e1805434031b5
edukempf <edukempf@gmail.com>
2013-02-15 17:26:46 -0200

    #32 Inclusão campo "data_validade" na tabela BEBIDAS e na tabela COMIDAS

--- Documentos/ScriptSql.sql
+++ Documentos/ScriptSql.sql
@@ -1,5 +1,5 @@
-﻿DROP DATABASE IF EXISTS aluno;
-CREATE DATABASE IF NOT EXISTS aluno ;
+﻿/*DROP DATABASE IF EXISTS aluno;
+CREATE DATABASE IF NOT EXISTS aluno ;          */
 
 USE aluno;
 
@@ -8,6 +8,7 @@ CREATE TABLE Comidas
 		(id INT NOT NULL AUTO_INCREMENT,
 		nome VARCHAR(50) NOT NULL,
     quantidade INT(3) NOT NULL,
+    data_validade DATE NOT NULL,
 		tipo ENUM('Kg','g') NOT NULL,
 		PRIMARY KEY (id))
 ENGINE = InnoDB;
@@ -18,7 +19,7 @@ CREATE TABLE Funcionario
 		(id INT NOT NULL AUTO_INCREMENT,
 		nome VARCHAR(100) NOT NULL,
 		cpf VARCHAR(11) UNIQUE NOT NULL,
-    rg VARCHAR(15) UNIQUE NOT NULL,
+    rg VARCHAR(15) NOT NULL,
 		data_nascimento DATE NOT NULL,
     estadocivil ENUM('Solteiro','Casado','Viúvo','Divorsiado') NOT NULL,
 		funcao VARCHAR(40) NOT NULL,
@@ -31,6 +32,7 @@ CREATE TABLE Bebidas
 		tipo ENUM ('Suco','Refrigerante','Alcoolica') NOT NULL,
 		nome VARCHAR(50) NOT NULL,
     quantidade INT(3) NOT NULL,
+    data_validade DATE,
     preco DOUBLE(5,2) NOT NULL,
 		PRIMARY KEY (id))
 ENGINE = InnoDB;
