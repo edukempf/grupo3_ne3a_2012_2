0f0532a47cdced5992a0d5404655b515fe08fd66
julianaIora <julianaiora@hotmail.com>
2013-04-08 10:13:46 -0300

    #42 Classe mesa com anotações alteradas

--- sistemaBarzinho/src/Modelo/Mesa.java
+++ sistemaBarzinho/src/Modelo/Mesa.java
@@ -4,17 +4,23 @@
  */
 package Modelo;
 
+import java.io.Serializable;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author Juliana
  */
-public class Mesa {
+@Entity
+@Table(name = "mesa")
+public class Mesa implements Serializable{
  @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @Column(length = 100)
     private int capacidade;    
