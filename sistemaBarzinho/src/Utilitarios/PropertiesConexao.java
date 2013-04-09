8dc0a539316d6543689bb706ff4db9d5bce7dcbd
edukempf <edukempf@gmail.com>
2013-04-09 14:19:47 -0300

    #46 Criada as classes para administrar os dados da conexão com o banco de dados

---
+++ sistemaBarzinho/src/Utilitarios/PropertiesConexao.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class PropertiesConexao {
+
+    public static Properties getProp() {
+        Properties prop = new Properties();
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream("./src/Utilitarios/conexao.properties");
+            prop.load(file);
+        } catch (IOException ex) {
+            Logger.getLogger(PropertiesConexao.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                file.close();
+            } catch (IOException ex) {
+                Logger.getLogger(PropertiesConexao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return prop;
+    }
+    
+    public static boolean saveProp(Properties prop){
+        FileOutputStream file = null;
+        boolean teste=false;
+        try {
+            file = new FileOutputStream("./src/Utilitarios/conexao.properties");
+            prop.store(file, null);
+            teste=true;
+        } catch (IOException ex) {
+            Logger.getLogger(PropertiesConexao.class.getName()).log(Level.SEVERE, null, ex);
+            
+        } finally {
+            try {
+                file.close();
+            } catch (IOException ex) {
+                Logger.getLogger(PropertiesConexao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+            return teste;
+    }
+}
