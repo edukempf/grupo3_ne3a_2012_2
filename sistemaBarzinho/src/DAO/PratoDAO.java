d7c7c8a695660596914c285d86e82cc38d408c9c
julianaIora <julianaiora@hotmail.com>
2013-04-11 11:37:37 -0300

    #41 DAO de Prato

---
+++ sistemaBarzinho/src/DAO/PratoDAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+import Modelo.Prato;
+import java.util.List;
+/**
+ *
+ * @author Ligue01
+ */
+public class PratoDAO {
+     public PratoDAO() {
+        super(Prato.class);
+    }
+    
+    public List<Prato> buscaPorNome(String nome){
+        session=HibernateConexao.iniciaConexao();
+        session.beginTransaction().begin();
+        List list = session.createQuery("from Prato as prato where prato.nome like '%"+nome+"%'").list();
+        session.beginTransaction().commit();
+        HibernateConexao.fechaConexao(session);
+        return list;
+    }
+}
