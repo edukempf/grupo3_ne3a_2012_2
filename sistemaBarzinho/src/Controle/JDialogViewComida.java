f114e9a38e75bf5aed131ecf1a877c873db58b73
julianaIora <julianaiora@hotmail.com>
2013-04-11 08:39:16 -0300

    #48 Alteração na tela de visualização de comida

--- sistemaBarzinho/src/Controle/JDialogViewComida.java
+++ sistemaBarzinho/src/Controle/JDialogViewComida.java
@@ -198,7 +198,7 @@ public class JDialogViewComida extends javax.swing.JDialog {
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                JDialogViewComida dialog = new JDialogViewComida(new javax.swing.JFrame(), true);
+                JDialogViewComida dialog = new JDialogViewComida(new javax.swing.JFrame(), true,null);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
