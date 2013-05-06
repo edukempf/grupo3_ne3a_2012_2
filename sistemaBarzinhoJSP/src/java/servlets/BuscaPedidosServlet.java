40137bff74c9d7121f4bef35cdbf4cd29df24022
julianaIora <julianaiora@hotmail.com>
2013-05-06 10:57:06 -0300

    #72 alteração em JSP

--- sistemaBarzinhoJSP/src/java/servlets/BuscaPedidosServlet.java
+++ sistemaBarzinhoJSP/src/java/servlets/BuscaPedidosServlet.java
@@ -10,17 +10,12 @@ import Modelos.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Eduardo
- */
 @WebServlet(name = "BuscaPedidosServlet", urlPatterns = {"/BuscaPedidosServlet"})
 public class BuscaPedidosServlet extends HttpServlet {
 
